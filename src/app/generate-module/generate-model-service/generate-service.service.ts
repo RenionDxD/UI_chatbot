21e52f1ce577a989561a95483dcfe857e6d52f1a
RenionDxD <ricardodiazzepeda064@gmail.com>
2023-11-15 09:34:57 -0600

    diseños

--- src/app/generate-module/generate-model-service/generate-service.service.ts
+++ src/app/generate-module/generate-model-service/generate-service.service.ts
@@ -8,9 +8,15 @@ import { Observable } from 'rxjs';
 export class GenerateServiceService {
 
   private apiUrl = 'http://127.0.0.1:5051/chatbot/modelar'; 
+  private apiUrl2 = 'http://127.0.0.1:5051/chatbot/historial'; 
 
 
   constructor(private http: HttpClient) { }
+
+  postChangeHistory(value: any): Observable<any> {
+    const body = { "boolean": value };
+    return this.http.post(this.apiUrl2,  body );
+  }
 
   postWithoutData(): Observable<any> {
     // No se envía ningún dato en el cuerpo de la solicitud POST
--- src/app/generate-module/generate-model/generate-model.component.ts
+++ src/app/generate-module/generate-model/generate-model.component.ts
@@ -9,21 +9,18 @@ import { GenerateServiceService } from '../generate-model-service/generate-servi
 export class GenerateModelComponent {
   progress = 0;
   response = '';
+
   constructor(private generateService: GenerateServiceService) { }
   generarModelo(){
-
     this.generateService.postWithoutData().subscribe(
       response => {
-
         this.progress = 0;
         const interval = setInterval(() => {
           this.progress += 10;
           if (this.progress >= 100) {
             clearInterval(interval);
             this.response = response;
-          }
-        }, 800);
-
+            }}, 800);
       },
       error => {
         alert(error);
@@ -31,6 +28,45 @@ export class GenerateModelComponent {
       }
     )
   }
+
+
+  isSwitchDisabled: boolean = false;
+  ngOnInit() {
+    // Inicializamos el switch como habilitado
+    this.isSwitchDisabled = false;
+    
+  }
+
+  onToggle(event: any) {
+
+    if (!this.isSwitchDisabled) {
+      // Aquí puedes manejar la lógica cuando se cambia el switch
+      // ...
+
+      // Deshabilitamos el switch durante 30 segundos
+      this.isSwitchDisabled = true;
+
+      // Habilitamos el switch después de 30 segundos
+      setTimeout(() => {
+        this.isSwitchDisabled = false;
+      }, 10000);
+    }
+
+    const value = event.target.checked;
+    this.generateService.postChangeHistory(value).subscribe(
+      response => {
+        console.log("Estado del interrptor correcto")
+      },
+      error => {
+        alert('Error al enviar el estado del interruptor:');
+      }
+    );
+  }
+
+
+
+
+
 
 
   
